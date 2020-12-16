ba61a0de275f23f0729c3d9a5e4adc3c424d5a3d
adqn <57331120+adqn@users.noreply.github.com>
2020-12-16 11:16:09 -0700

    adding post functionality

--- api/api.js
+++ api/api.js
@@ -3,6 +3,22 @@ const path = require('path')
 const express = require('express')
 const sqlite3 = require('sqlite3')
 
+const schema = `CREATE TABLE IF NOT EXISTS post ( 
+    postId INTEGER NOT NULL AUTOINCREMENT,
+    threadId INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    postName TEXT NOT NULL,
+    comment TEXT NOT NULL,
+    FOREIGN KEY (threadId) 
+    REFERENCES threads (threadId)
+      ON UPDATE CASCADE
+      ON DELETE CASCADE
+);
+CREATE TABLE IF NOT EXISTS threads (
+    threadId INTEGER NOT NULL,
+    opComment TEXT NOT NULL
+);`
+
 const testPost = [
   {
     boardId: 1,
@@ -29,6 +45,22 @@ let db = new sqlite3.Database('./api/db/board.db', (err) => {
   } else {
     console.log("Board database up")
   }
+
+  db.exec('PRAGMA foreign_keys = ON;', (err) => {
+    if (err) {
+      console.log("Couldn't enable foreign keys")
+    } else {
+      console.log("Foreign keys enabled")
+    }
+  })
+})
+
+db.serialize(() => {
+  db.exec(schema, err => {
+    if (err) {
+      console.log("Unable to import database schema")
+    }
+  })
 })
 
 //
