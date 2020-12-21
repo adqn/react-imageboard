fdde24d60b592007d3eeee126d3a48ed28dc9c0b
adqn <57331120+adqn@users.noreply.github.com>
2020-12-20 22:09:42 -0700

    post functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useReducer } from 'react';
+import Post from './components/Post.js';
 import './App.css';
 
 const api = option => "http://localhost:5001/api/" + option;
@@ -6,20 +7,22 @@ const api = option => "http://localhost:5001/api/" + option;
 const Posts = ({ posts }) =>
   <div className="Post">
     {posts.map(post =>
-      <ul>
-        <li>{post.globalId}</li>
-        <li>{post.name}</li>
-        <li>{post.comment}</li>
-      </ul>
+      <Post 
+        id={post.postId}  
+        timestamp={post.created}
+        name={post.postName}
+        comment={post.comment}
+      />
     )}
   </div>
 
 function Thread() {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [countdownTime, setCountdown] = useState(5);
 
   const retrievePosts = () => (
-    fetch('http://localhost:5001/api/testpost')
+    fetch('http://localhost:5001/api/getposts')
       .then(response => response.json())
       .then(response => {
           setPosts(response)
@@ -27,11 +30,25 @@ function Thread() {
       })
   )
 
+  const countdown = (timeFrom, callback) => {
+    if (countdownTime === 1) {
+      callback();
+      setCountdown(timeFrom);
+    } else (
+      setCountdown(countdownTime - 1)
+    )
+  }
+
   useEffect(
     () => {
       setIsLoading(true);
       retrievePosts();
   }, []);
+
+  useEffect(
+    () => {
+      setTimeout(() => countdown(5, () => retrievePosts()), 1000);
+  }, [countdownTime]);
 
   return (
     <div className="ThreadContainer">
@@ -39,6 +56,7 @@ function Thread() {
       <p>Loading posts...</p>
       :
       <Posts posts={posts} />}
+      {countdownTime}
     </div>
   )
 }
@@ -98,10 +116,21 @@ function ReplyForm() {
 }
 
 function App() {
+  const [newPost, setNewPost] = useState(false);
+
+  function postsReducer (state, action) {
+    switch(action.type) {
+      case 'DELETE_POST':
+        return action.payload;
+      case 'UPDATE_POST':
+        //
+    }
+  }
+
   return (
     <div>
-      <ReplyForm />
-      <Thread />
+      {/* <ReplyForm setNewPost={setNewPost} /> */}
+      <Thread newPost={newPost} />
     </div>
   )
 }
