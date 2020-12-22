ec721b0bfb64b2b54e96bb5d5ca78fe3c708c82b
adqn <57331120+adqn@users.noreply.github.com>
2020-12-22 06:02:01 -0700

    updating schema

--- api/schema.sql
+++ api/schema.sql
@@ -1,24 +1,40 @@
-CREATE TABLE IF NOT EXISTS posts ( 
-    postId INTEGER PRIMARY KEY AUTOINCREMENT,
-    threadId INTEGER,
+CREATE TABLE IF NOT EXISTS posts_b ( 
+    post INTEGER NOT NULL UNIQUE AUTOINCREMENT,
+    thread INTEGER DEFAULT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    email TEXT DEFAULT NULL,
     postName TEXT,
-    comment TEXT NOT NULL,
-    FOREIGN KEY (threadId) 
-    REFERENCES threads (threadId)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
+    comment TEXT,
+    file TEXT DEFAULT NULL
 );
 
-CREATE TABLE IF NOT EXISTS threads (
-    -- boardId INTEGER,
-    threadId INTEGER PRIMARY KEY AUTOINCREMENT,
-    opComment TEXT NOT NULL
-    -- FOREIGN KEY (boardId)
-    -- REFERENCES boards (boardId)
-    --     ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS posts_know ( 
+    post INTEGER NOT NULL UNIQUE AUTOINCREMENT,
+    thread INTEGER DEFAULT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    email TEXT DEFAULT NULL,
+    postName TEXT,
+    comment TEXT,
+    file TEXT DEFAULT NULL
 );
 
--- CREATE TABLE IF NOT EXISTS boards (
---     boardId INTEGER PRIMARY KEY AUTOINCREMENT,
+-- CREATE TABLE IF NOT EXISTS threads (
+--     -- boardId INTEGER,
+--     threadId INTEGER PRIMARY KEY AUTOINCREMENT,
+--     opComment TEXT NOT NULL
+--     -- FOREIGN KEY (boardId)
+--     -- REFERENCES boards (boardId)
+--     --     ON DELETE CASCADE
 -- );
+
+CREATE TABLE IF NOT EXISTS boards (
+    uri TEXT
+    boardId INTEGER PRIMARY KEY AUTOINCREMENT,
+    description TEXT,
+);
+-- 
+INSERT INTO boards VALUES
+('b', 'Random', NULL)
+
+INSERT INTO boards VALUES
+('know', 'Knowledge and Information', NULL)
