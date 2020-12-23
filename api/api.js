2f673e7e19b13b7cabc6580beb7ea388c2a97130
adqn <57331120+adqn@users.noreply.github.com>
2020-12-23 12:33:43 -0700

    prettying

--- api/api.js
+++ api/api.js
@@ -1,34 +1,35 @@
-const path = require('path')
-const express = require('express')
-const sqlite3 = require('sqlite3')
-const fs = require('fs')
-const url = require('url');
+const path = require("path");
+const express = require("express");
+const sqlite3 = require("sqlite3");
+const fs = require("fs");
+const url = require("url");
 
-let db = new sqlite3.Database('./api/db/db.db', (err) => {
+let db = new sqlite3.Database("./api/db/db.db", (err) => {
   if (err) {
-    console.log("Unable to open board database: \n" +  "\t" + err.message)
+    console.log("Unable to open board database: \n" + "\t" + err.message);
   } else {
-    console.log("Board database up")
+    console.log("Board database up");
   }
-})
+});
 
-const createDb = () => new Promise((resolve, reject) =>
- fs.readFile( path.join(__dirname, "./schema.sql"),
-  (err, data) => {
-   if (err) return reject(err);
+const createDb = () =>
+  new Promise((resolve, reject) =>
+    fs.readFile(path.join(__dirname, "./schema.sql"), (err, data) => {
+      if (err) return reject(err);
 
-   db.exec(data.toString(), (err) => {
-    if (err) return reject(err);
-    resolve();
-   })
-  }))
+      db.exec(data.toString(), (err) => {
+        if (err) return reject(err);
+        resolve();
+      });
+    })
+  );
 
 //
 // post functions
 //
 
 function newPost(post, callback) {
-  let {thread, email, name, comment, file} = post;
+  let { thread, email, name, comment, file } = post;
   const sql = `INSERT INTO posts_${board} 
                    VALUES 
                    (null,
@@ -38,18 +39,19 @@ function newPost(post, callback) {
                     "${email}",
                     "${name}", 
                     "${comment}",
-                    "${file}");`
+                    "${file}");`;
 
-  db.run(sql, err => {
-    if (err) {console.log(err)}
-    else {
+  db.run(sql, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
       () => callback.sendStatus(200);
     }
-  })
+  });
 }
 
 function newThread(board, post) {
-  let {subject, email, name, comment} = post;
+  let { subject, email, name, comment } = post;
   const sql = `INSERT INTO posts_${board} 
                      VALUES 
                      (NULL,
@@ -59,40 +61,40 @@ function newThread(board, post) {
                       "${email}",
                       "${name}",
                       "${comment}",
-                      NULL);`
+                      NULL);`;
 
-  let maxThread = 'SELECT * FROM threads ORDER BY thread DESC LIMIT 1;';
+  let maxThread = "SELECT * FROM threads ORDER BY thread DESC LIMIT 1;";
   let newThread;
 
   db.serialize(() => {
-    db.get(maxThread, row => {
+    db.get(maxThread, (row) => {
       newThread = row.thread + 1;
-    })
+    });
 
-    db.exec(sql)
-  })
+    db.exec(sql);
+  });
 }
 
 function getPosts(req, callback) {
-  let {query, board, thread, post} = req;
+  let { query, board, thread, post } = req;
   let sql;
 
-  if (query === 'post') {
+  if (query === "post") {
     sql = `SELECT * FROM posts_${board}
                         WHERE post = ${post}`;
   }
 
-  if (query === 'thread') {
+  if (query === "thread") {
     sql = `SELECT * FROM posts_${board} 
                         WHERE
                         thread = ${thread}`;
   }
 
-  if (query === 'threads') {
+  if (query === "threads") {
     sql = `SELECT * FROM posts_${board} GROUP BY thread`;
   }
 
-  if (query === 'opPost') {
+  if (query === "opPost") {
     sql = `SELECT * FROM posts_${board}
               GROUP BY thread`;
   }
@@ -100,10 +102,14 @@ function getPosts(req, callback) {
   let result = [];
 
   db.serialize(() => {
-    db.each(sql, (err, row) => {
-      result.push(row);
-    }, () => callback.send(result))
-  })
+    db.each(
+      sql,
+      (err, row) => {
+        result.push(row);
+      },
+      () => callback.send(result)
+    );
+  });
 }
 
 const getBoards = (callback) => {
@@ -111,60 +117,68 @@ const getBoards = (callback) => {
   let result = [];
 
   db.serialize(() => {
-    db.each(sql, (err, row) => {        
-      result.push(row);
-    }, () => callback.send(result))
-  })
-}
-
+    db.each(
+      sql,
+      (err, row) => {
+        result.push(row);
+      },
+      () => callback.send(result)
+    );
+  });
+};
 
 //
 // routes
 //
 
-const app = express() 
+const app = express();
 
-app.use(express.urlencoded())
-app.use(express.json())
+app.use(express.urlencoded());
+app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-  next()
-})
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
-app.get('/test', (req, res) => {
-  res.writeHead(200, {'content-type': 'text/html'})
-  fs.createReadStream(__dirname + '/test.html').pipe(res)
-})
+app.get("/test", (req, res) => {
+  res.writeHead(200, { "content-type": "text/html" });
+  fs.createReadStream(__dirname + "/test.html").pipe(res);
+});
 
-app.get('/api/newthread', (req, res) => {
-  newThread(req.body)
-  res.sendStatus(200)
-})
+app.get("/api/newthread", (req, res) => {
+  newThread(req.body);
+  res.sendStatus(200);
+});
 
-app.get('/api/testpost', (req, res) => {
-  res.send(JSON.stringify(testPost))
-})
+app.get("/api/testpost", (req, res) => {
+  res.send(JSON.stringify(testPost));
+});
 
-app.post('/api/newpost', (req, res) => {
-  console.log(req.body)
-  newPost(req.body)
+app.post("/api/newpost", (req, res) => {
+  console.log(req.body);
+  newPost(req.body);
   //.then
-  res.sendStatus(200)
-})
+  res.sendStatus(200);
+});
 
 // query for thread ID (no separate boards for now)
-app.get('/api/getposts', (req, res) => {
+app.get("/api/getposts", (req, res) => {
   const query = url.parse(req.url, true).query;
   getPosts(query, res);
   // res.sendStatus(200)
-})
+});
 
-app.get('/api/getboards', (req, res) => {
+app.get("/api/getboards", (req, res) => {
   getBoards(res);
-})
+});
 const port = 5001;
-const server = app.listen(port, () => console.log("Server listening on port " + port))
+const server = app.listen(port, () =>
+  console.log("Server listening on port " + port)
+);
 
 createDb();
 
