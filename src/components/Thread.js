ae9b20f7e70ddb06cd840184a252523fa6b5be32
adqn <57331120+adqn@users.noreply.github.com>
2020-12-24 22:05:24 -0700

    fixed misc posting

--- src/components/Thread.js
+++ src/components/Thread.js
@@ -1,62 +1,58 @@
-import {React, useEffect, useState} from 'react';
-import Post from './Post';
+import { React, useEffect, useState } from "react";
+import Post from "./Post";
+import ReplyForm from './ReplyForm';
 
-const Posts = ({ posts }) =>
+const Posts = ({ posts }) => (
   <div className="Post">
-    {posts.map(post =>
-      <Post 
-        id={post.postId}  
+    {posts.map((post) => (
+      <Post
+        subject={post.subject}
+        id={post.post}
         timestamp={post.created}
-        name={post.postName}
+        name={post.name}
         comment={post.comment}
       />
-    )}
+    ))}
   </div>
+);
 
-function Thread() {
+function Thread({ uri, id }) {
   const [posts, setPosts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [countdownTime, setCountdown] = useState(5);
 
-  const retrievePosts = () => (
-    fetch('http://localhost:5001/api/getposts')
-      .then(response => response.json())
-      .then(response => {
-          setPosts(response)
-          // setThreadId(response.threadId)
-          setIsLoading(false)
-      })
-  )
+  const reqString = `/?query=thread&board=${uri}&thread=${id}&post=null`;
+
+  const retrievePosts = () =>
+    fetch("http://localhost:5001/api/getposts" + reqString)
+      .then((response) => response.json())
+      .then((response) => {
+        setPosts(response);
+        setIsLoading(false);
+      });
 
   const countdown = (timeFrom, callback) => {
     if (countdownTime === 1) {
       callback();
       setCountdown(timeFrom);
-    } else (
-      setCountdown(countdownTime - 1)
-    )
-  }
+    } else setCountdown(countdownTime - 1);
+  };
 
-  useEffect(
-    () => {
-      setIsLoading(true);
-      retrievePosts();
+  useEffect(() => {
+    setIsLoading(true);
+    retrievePosts();
   }, []);
 
-  useEffect(
-    () => {
-      setTimeout(() => countdown(5, () => retrievePosts()), 1000);
+  useEffect(() => {
+    setTimeout(() => countdown(5, () => retrievePosts()), 1000);
   }, [countdownTime]);
 
   return (
     <div className="ThreadContainer">
-      {isLoading ? 
-      <p>Loading posts...</p>
-      :
-      <Posts posts={posts} />}
-      {countdownTime}
+      <Posts posts={posts} />
+      {isLoading ? null : countdownTime}
     </div>
-  )
+  );
 }
 
 export default Thread;
