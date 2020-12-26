55ce6910e3d9feccec72247def7e7e0f229f0c87
adqn <57331120+adqn@users.noreply.github.com>
2020-12-26 16:01:41 -0700

    added image processing helpers

---
+++ api/images.js
@@ -0,0 +1,37 @@
+const fs = require('fs')
+  , gm = require('gm').subClass({imageMagick: true})
+
+const resize = imageName => {
+  function callback(size) {
+    let thumb_w;
+    let thumb_h;
+
+    let ratio = (size.width > size.height ? 250/size.width : 250/size.height)
+
+    thumb_w = size.width * ratio;
+    thumb_h = size.height * ratio;
+
+    let name = imageName.match(/\d+/)[0]
+    let ext = imageName.match(/\..+/)[0]
+
+    image
+      .resize(thumb_w, thumb_h)
+      .noProfile()
+      .autoOrient()
+      .write(__dirname + '/img/' + name + 's' + ext, (err) => err ? console.log(err) : null)
+  }
+
+  image = gm(__dirname + '/img/' + imageName);
+
+  let size = {};
+  let sizeObj = image.size((err, value) => {
+    size.width = value.width;
+    size.height = value.height;
+    callback(size)
+  });
+
+}
+
+// resize('143682248658.png')
+
+exports.resize = resize;
