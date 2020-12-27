b9317acf0990b6a5517909fd784d6e44d2e6beb5
adqn <57331120+adqn@users.noreply.github.com>
2020-12-27 13:05:26 -0700

    refactoring

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import yukkuri from "../static/yukkuri.png";
+
+  const Home = () =>
+    <div>
+      <div className="header">
+        <span id="homename">soupchan</span>
+        <br />
+        <img src={yukkuri} />
+        <br />
+        <span id="imgtxt">
+          <i>"Take it easy!"</i>
+        </span>
+      </div>
+      <div className="Home"></div>
+      <div className="News"></div>
+      {/* {boards.map(board => {
+        <a href={`/${board.uri}`}>{` /${board.uri} `}</a> 
+      })} */}
+    </div>
+
+export default Home;
