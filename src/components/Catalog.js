3246c2b3fe4c1cb390ce22087249a9efef3d5cbf
adqn <57331120+adqn@users.noreply.github.com>
2021-01-03 13:50:38 -0700

    misc

--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import ReplyArea from './ReplyArea';
+import { formatComment } from '../helpers/postHelpers.js';
 
 const api = option => "http://localhost:5001/api/" + option
 
@@ -12,29 +13,37 @@ const getThumb = fileName => {
 
 const OpPost = ({ post }) => {
   let {
+    thread,
     subject,
     file,
     fileWidth,
     fileHeight,
-    comment
   } = post;
   let thumb_h;
   let thumb_w;
   let ratio;
+  // let comment = formatComment(post.comment);
 
-  ratio = (fileWidth > fileHeight ? 150 / fileWidth : 150 / fileHeight);
-  thumb_w = fileWidth * ratio;
-  thumb_h = fileHeight * ratio;
+  if (fileWidth > 150 || fileHeight > 150) {
+    ratio = (fileWidth > fileHeight ? 150 / fileWidth : 150 / fileHeight);
+    thumb_w = fileWidth * ratio;
+    thumb_h = fileHeight * ratio;
+  } else {
+    thumb_w = fileWidth;
+    thumb_h = fileHeight;
+  }
 
   return (
       <div class="catalog opPost">
-      <a href={"./thread/" + post.thread}>
-        <img src={"http://localhost:5001/img/" + getThumb(post.file)}
+      <a href={"./thread/" + thread}>
+        <img src={"http://localhost:5001/img/" + getThumb(file)}
           style={{width: thumb_w, height: thumb_h}}/>
       </a>
         <span class="catalog threadInfo">471 / 3 / 6</span>
-      <span class="catalog threadSubject">{post.subject}</span>
-      {post.comment}
+      <span class="catalog threadSubject">{subject}</span>
+      <div class="postMessage">
+      {formatComment(post.comment)}
+      </div>
       </div>
   )
 }
@@ -49,14 +58,18 @@ const Catalog = ({uri}) => {
       .then(resp => setThreads(resp))
   }
 
-
   useEffect(() => getThreads(), [])
 
   return (
     <div>
       <ReplyArea index={true} uri={uri} id={null} />
       <hr />
-      {threads ? threads.map(post => <OpPost post={post} />) : null}
+      <div class="navLinks">[<a href="../../">Home</a>] [<a href={"/" + uri}>Index</a>] [<a href="#update"
+        onClick={() => getThreads()}>Update</a>]</div>
+      <hr />
+      <div class="catalog container">
+        {threads ? threads.map(post => <OpPost post={post} />) : null}
+      </div>
     </div>
   )
 }
