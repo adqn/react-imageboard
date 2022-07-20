5eb5f05c9d100749ca9a0bf334d8c0330464b252
adqn <57331120+adqn@users.noreply.github.com>
2022-07-20 12:28:01 -0700

    Set up unit testing for Home

---
+++ client/src/components/Home.test.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM, { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import Home from "./Home";
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+describe("Home component testing", () => {
+  it("Renders with welcome text", () => {
+    act(() => {
+      render(<Home />, container);
+    });
+    const span = container.querySelector("#imgtxt");
+    expect(span.textContent).toBe('"Take it easy!"');
+  });
+});
